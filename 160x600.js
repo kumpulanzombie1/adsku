15296097c9606bdbb1766e519b48b99f868a00e7
kumpulanzombie1 <148730992+kumpulanzombie1@users.noreply.github.com>
2023-10-23 09:25:16 +0800

    Update 160x600.js

--- 160x600.js
+++ 160x600.js
@@ -1,5 +1,4 @@
-<script type="text/javascript">
-	atOptions = {
+atOptions = {
 		'key' : '765461cee3e28eee943f2ab5c2899fb5',
 		'format' : 'iframe',
 		'height' : 600,
@@ -7,4 +6,3 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/765461cee3e28eee943f2ab5c2899fb5/invoke.js"></scr' + 'ipt>');
-</script>
