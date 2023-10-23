27a21a58bc4a797ad90e72c192711418812f24f4
kumpulanzombie1 <148730992+kumpulanzombie1@users.noreply.github.com>
2023-10-23 09:25:00 +0800

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -1,5 +1,4 @@
-<script type="text/javascript">
-	atOptions = {
+atOptions = {
 		'key' : '796a6ad766232eaf6e672a8b52be774c',
 		'format' : 'iframe',
 		'height' : 250,
@@ -7,4 +6,3 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/796a6ad766232eaf6e672a8b52be774c/invoke.js"></scr' + 'ipt>');
-</script>
