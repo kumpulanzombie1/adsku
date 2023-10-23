b97c7f00b011c1938d3c6e8fbfb17da1c3aa0f35
kumpulanzombie1 <148730992+kumpulanzombie1@users.noreply.github.com>
2023-10-23 09:25:27 +0800

    Update 728x90.js

--- 728x90.js
+++ 728x90.js
@@ -1,5 +1,4 @@
-<script type="text/javascript">
-	atOptions = {
+atOptions = {
 		'key' : '08b743e6ea26abfac4b7e648991160d2',
 		'format' : 'iframe',
 		'height' : 90,
@@ -7,4 +6,3 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/08b743e6ea26abfac4b7e648991160d2/invoke.js"></scr' + 'ipt>');
-</script>
