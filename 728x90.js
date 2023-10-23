e6331c7cdf92689490b771547f781f048bfde0c3
kumpulanzombie1 <148730992+kumpulanzombie1@users.noreply.github.com>
2023-10-23 09:18:43 +0800

    Create 728x90.js

---
+++ 728x90.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '08b743e6ea26abfac4b7e648991160d2',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/08b743e6ea26abfac4b7e648991160d2/invoke.js"></scr' + 'ipt>');
+</script>
